65d61760b3115d15618e54d33daf6cf593c193a1
hafsa-11 <hafsa98lakhouaja@gmail.com>
2024-01-09 12:34:22 +0100

    upload

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,5 @@
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Save this script as 0-privileges.sql
+SELECT CONCAT('Grants for ', user, '@', host) AS UserGrants
+FROM mysql.user
+WHERE user IN ('user_0d_1', 'user_0d_2')
+  AND host = 'localhost';
