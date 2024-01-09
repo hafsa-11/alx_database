b00c29c098ff4cb3d0ab2ffada21883cde120554
hafsa-11 <hafsa98lakhouaja@gmail.com>
2024-01-09 14:47:06 +0100

    upload

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Delete user user_0d_1 and user_0d_2 if they exist
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
+DROP USER IF EXISTS 'user_0d_2'@'localhost';1
 
