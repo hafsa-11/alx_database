e7f950f0b70369e2285788e83cef066b588a7a5d
hafsa-11 <hafsa98lakhouaja@gmail.com>
2024-01-10 09:12:00 +0100

    up

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,9 @@
--- 0-privileges.sql
+-- main_0_0.sql
 
--- Grant privileges for user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Grant privileges for user_0d_1 only if the user doesn't exist
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'password' REQUIRE NONE;
 
--- Grant privileges for user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- main_2_0.sql
+
+-- Attempt to create user_0d_1 with GRANT, expecting an error
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
